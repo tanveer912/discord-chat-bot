348b8b59e53d076bcd3a1e617073386c7b6e2599
Tanveer <khansamson1@gmail.com>
2021-05-13 01:12:43 +0600

    some changes

--- index.js
+++ index.js
@@ -2,6 +2,32 @@ const Discord = require("discord.js")
 const fetch = require ("node-fetch")
 const client= new Discord.Client()
 
+
+
+const db = new Database()
+const client = new Discord.Client()
+
+const sadWords = ["sad", "depressed", "unhappy", "angry"]
+
+const starterEncouragements = [
+  "Cheer up!",
+  "Hang in there.",
+  "You are a great person / bot!"
+]
+
+db.get("encouragements").then(encouragements => {
+  if (!encouragements || encouragements.length < 1) {
+    db.set("encouragements", starterEncouragements)
+  }
+})
+
+db.get("responding").then(value => {
+  if (value == null) {
+    db.set("responding", true)
+  }
+})
+
+
 function getQuote(){
   return fetch ("https://zenquotes.io/api/random")
       .then(res =>{
