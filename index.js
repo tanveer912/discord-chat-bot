0f170fb578087b5fc54a925e00b86ce49421b818
Tanveer <khansamson1@gmail.com>
2021-05-28 22:40:38 +0600

    quote function

--- index.js
+++ index.js
@@ -64,6 +64,15 @@ function deleteEncouragement(index) {
 }
 
 
+function getQuote() {
+  return fetch("https://zenquotes.io/api/random")
+    .then(res => {
+      return res.json()
+    })
+    .then(data => {
+      return data[0]["q"] + " -" + data[0]["a"]
+    })
+}
 
 
 
